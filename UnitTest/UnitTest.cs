a9cce7d3ba155b94a00c644ca303a6ff8a9e69e6
Deyvi Josue Ortiz Vidaurre <DORTIZV@farmacorp.com>
2019-09-05 21:11:25 -0400

    Implementacion unit test registro transaccion monto 0.

--- UnitTest/UnitTest.cs
+++ UnitTest/UnitTest.cs
@@ -12,38 +12,48 @@ namespace UnitTest
         [TestMethod]
         public void UnitTestRegisterCustomer()
         {
+            var cust = new CustomerParam()
+            {
+                FirstName = "Josue",
+                SurName = "Ortiz",
+                Email = "djovidaurre@gmail.com",
+                DateOfBirth = new DateTime(1990, 12, 18)
+            };
+
             var customer = ConsultarApi(
-                new CustomerParam()
-                {
-                    FirstName = "Josue",
-                    SurName = "Ortiz",
-                    Email = "djovidaurre@gmail.com",
-                    DateOfBirth = new DateTime(1990, 12, 18)
-                }, recurso.RegistrarCliente
+                JsonConvert.SerializeObject(cust),
+                recurso.RegistrarCliente
                 );
+
+            var result = JsonConvert.DeserializeObject<CustomerDto>(customer.Content);
 
             var expected = true;
 
-            Assert.AreEqual(expected, customer.Response);
+            Assert.AreEqual(expected, result.Response);
 
         }
 
         [TestMethod]
         public void UnitTestRegisterCustomerWithOutSurName()
         {
+            var cust = new CustomerParam()
+            {
+                FirstName = "Josue",
+                SurName = "",
+                Email = "djovidaurre@gmail.com",
+                DateOfBirth = new DateTime(1990, 12, 18)
+            };
+
             var customer = ConsultarApi(
-                new CustomerParam()
-                {
-                    FirstName = "Josue",
-                    SurName = "",
-                    Email = "djovidaurre@gmail.com",
-                    DateOfBirth = new DateTime(1990, 12, 18)
-                }, recurso.RegistrarCliente
-                );
+             JsonConvert.SerializeObject(cust),
+             recurso.RegistrarCliente
+             );
+
+            var result = JsonConvert.DeserializeObject<CustomerDto>(customer.Content);
 
             var expected = false;
 
-            Assert.AreEqual(expected, customer.Response);
+            Assert.AreEqual(expected, result.Response);
 
         }
 
@@ -51,37 +61,47 @@ namespace UnitTest
         [TestMethod]
         public void UnitTestRegisterCustomerWithOutEmail()
         {
+            var cust = new CustomerParam()
+            {
+                FirstName = "Josue",
+                SurName = "Ortiz",
+                Email = "",
+                DateOfBirth = new DateTime(1990, 12, 18)
+            };
+
             var customer = ConsultarApi(
-                new CustomerParam()
-                {
-                    FirstName = "Josue",
-                    SurName = "Ortiz",
-                    Email = "",
-                    DateOfBirth = new DateTime(1990, 12, 18)
-                }, recurso.RegistrarCliente
-                );
+            JsonConvert.SerializeObject(cust),
+            recurso.RegistrarCliente
+            );
+
+            var result = JsonConvert.DeserializeObject<CustomerDto>(customer.Content);
 
             var expected = false;
 
-            Assert.AreEqual(expected, customer.Response);
+            Assert.AreEqual(expected, result.Response);
 
         }
 
         [TestMethod]
         public void UnitTestRegisterCustomerWithOutDateOfBirth()
         {
+            var cust = new CustomerParam()
+            {
+                FirstName = "Josue",
+                SurName = "Ortiz",
+                Email = "djovidaurre@gmail.com"
+            };
+
             var customer = ConsultarApi(
-                new CustomerParam()
-                {
-                    FirstName = "Josue",
-                    SurName = "Ortiz",
-                    Email = "djovidaurre@gmail.com"
-                }, recurso.RegistrarCliente
-                );
+             JsonConvert.SerializeObject(cust),
+             recurso.RegistrarCliente
+             );
+
+            var result = JsonConvert.DeserializeObject<CustomerDto>(customer.Content);
 
             var expected = true;
 
-            Assert.AreEqual(expected, customer.Response);
+            Assert.AreEqual(expected, result.Response);
 
         }
 
@@ -95,12 +115,12 @@ namespace UnitTest
                 Monto = 100
             };
             var transction = ConsultarApi(
-                JsonConvert.SerializeObject(trans), 
+                JsonConvert.SerializeObject(trans),
                 recurso.RegistrarTransaccion
                 );
 
 
-           var result = JsonConvert.DeserializeObject<TransactionDto>(transction.Content);
+            var result = JsonConvert.DeserializeObject<TransactionDto>(transction.Content);
 
             var expected = true;
 
@@ -108,24 +128,29 @@ namespace UnitTest
 
         }
 
-
-
-        public CustomerDto ConsultarApi(CustomerParam param, string urlApi)
+        [TestMethod]
+        public void UnitTestRegisterTransactionWithZeroBalance()
         {
-            var valorJson = JsonConvert.SerializeObject(param);
+            var trans = new TransactionParam()
+            {
+                IdBilletera = 1,
+                IdTipoTransaccion = 1,
+                Monto = 0
+            };
+            var transction = ConsultarApi(
+                JsonConvert.SerializeObject(trans),
+                recurso.RegistrarTransaccion
+                );
 
-            var url =  string.Format(recurso.UrlApiWallet, urlApi);
 
-            var client = new RestClient(url);
-            var request = new RestRequest(Method.POST);
-            request.AddHeader("cache-control", "no-cache");
-            request.AddHeader("Content-Type", "application/json");
-            request.AddParameter("undefined", valorJson, ParameterType.RequestBody);
-            IRestResponse response = client.Execute(request);
+            var result = JsonConvert.DeserializeObject<TransactionDto>(transction.Content);
 
-            return JsonConvert.DeserializeObject<CustomerDto>(response.Content);
+            var expected = false;
+
+            Assert.AreEqual(expected, result.Response);
 
         }
+
 
         public IRestResponse ConsultarApi(string json, string urlApi)
         {
